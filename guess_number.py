3d105246224e7470353a66b59bbb1d5ec9601849
Bryce W <58643544+02bwilson@users.noreply.github.com>
2022-05-05 12:38:34 -0400

    update

--- guess_number.py
+++ guess_number.py
@@ -4,7 +4,12 @@ s = random.randrange(1,50)
 print(s)
 guessed = False
 while(guessed == False):
-    a = int(input("Guess: "))
-    if (a==s):
+    try:
+        guess = input("Guess: ")
+        guessInt = int(guess)
+        
+    except Exception:
+        print(f"Sorry, {guess} is not a number")
+    if (guess==s):
         guessed = True
 print("Correct!")
